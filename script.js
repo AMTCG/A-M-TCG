73304a702e032a6b6d2e56930fe6f9e247210f33
A&M TCG <matenkovicalex@gmail.com>
2026-03-29 20:31:06 +0200

    Refactor functions and add card animations

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function scrollToGallery() {
 function goToTikTok() {
   window.open("https://www.tiktok.com/@ampokemon", "_blank");
 }
+const cards = document.querySelectorAll('.card-bg');
+
+// Presné štartové pozície (px od ľavého okraja)
+const positions = [50, window.innerWidth/2, window.innerWidth - 130];
+
+cards.forEach((card, index) => {
+  // horizontálna pozícia podľa indexu
+  card.style.left = positions[index] + 'px';
+  
+  // náhodná rýchlosť 5–15 sekúnd
+  const randomDuration = 5 + Math.random() * 10;
+  card.style.animationDuration = randomDuration + 's';
+});
